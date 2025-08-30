b8c87fb6e2eef1236939e5e05323edaa3ad929cb
vmenge <vfmenge@gmail.com>
2025-08-30 07:05:42 +0200

    dconf

--- home/dconf.nix
+++ home/dconf.nix
@@ -5,13 +5,26 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
+    "com/github/ryonakano/reco" = {
+      autosave-destination = "/home/vmenge/Downloads";
+    };
+
     "org/gnome/control-center" = {
       last-panel = "keyboard";
-      window-state = mkTuple [ 1280 784 false ];
+      window-state = mkTuple [
+        1280
+        784
+        false
+      ];
     };
 
     "org/gnome/desktop/app-folders" = {
-      folder-children = [ "System" "Utilities" "YaST" "Pardus" ];
+      folder-children = [
+        "System"
+        "Utilities"
+        "YaST"
+        "Pardus"
+      ];
     };
 
     "org/gnome/desktop/app-folders/folders/Pardus" = {
@@ -21,11 +34,25 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/app-folders/folders/System" = {
+      apps = [
+        "org.gnome.baobab.desktop"
+        "org.gnome.DiskUtility.desktop"
+        "org.gnome.Logs.desktop"
+        "org.gnome.SystemMonitor.desktop"
+      ];
       name = "X-GNOME-Shell-System.directory";
       translate = true;
     };
 
     "org/gnome/desktop/app-folders/folders/Utilities" = {
+      apps = [
+        "org.gnome.Connections.desktop"
+        "org.gnome.Evince.desktop"
+        "org.gnome.FileRoller.desktop"
+        "org.gnome.font-viewer.desktop"
+        "org.gnome.Loupe.desktop"
+        "org.gnome.seahorse.Application.desktop"
+      ];
       name = "X-GNOME-Shell-Utilities.directory";
       translate = true;
     };
@@ -46,8 +73,13 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/input-sources" = {
-      sources = [ (mkTuple [ "xkb" "us" ]) ];
-      xkb-options = [];
+      sources = [
+        (mkTuple [
+          "xkb"
+          "us"
+        ])
+      ];
+      xkb-options = [ ];
     };
 
     "org/gnome/desktop/interface" = {
@@ -57,6 +89,10 @@ with lib.hm.gvariant;
 
     "org/gnome/desktop/notifications" = {
       application-children = [ "steam" ];
+    };
+
+    "org/gnome/desktop/notifications/application/gnome-power-panel" = {
+      application-id = "gnome-power-panel.desktop";
     };
 
     "org/gnome/desktop/notifications/application/steam" = {
@@ -72,12 +108,16 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/search-providers" = {
-      sort-order = [ "org.gnome.Settings.desktop" "org.gnome.Contacts.desktop" "org.gnome.Nautilus.desktop" ];
+      sort-order = [
+        "org.gnome.Settings.desktop"
+        "org.gnome.Contacts.desktop"
+        "org.gnome.Nautilus.desktop"
+      ];
     };
 
     "org/gnome/desktop/wm/keybindings" = {
       close = [ "<Super>q" ];
-      minimize = [];
+      minimize = [ ];
       move-to-workspace-1 = [ "<Shift><Super>1" ];
       move-to-workspace-2 = [ "<Shift><Super>2" ];
       move-to-workspace-3 = [ "<Shift><Super>3" ];
@@ -87,7 +127,7 @@ with lib.hm.gvariant;
       move-to-workspace-7 = [ "<Shift><Super>7" ];
       move-to-workspace-8 = [ "<Shift><Super>8" ];
       move-to-workspace-9 = [ "<Shift><Super>9" ];
-      move-to-workspace-10 = [ "<Shift><Super>10" ];
+      move-to-workspace-10 = [ "<Shift><Super>0" ];
       switch-to-workspace-1 = [ "<Super>1" ];
       switch-to-workspace-2 = [ "<Super>2" ];
       switch-to-workspace-3 = [ "<Super>3" ];
@@ -97,10 +137,14 @@ with lib.hm.gvariant;
       switch-to-workspace-7 = [ "<Super>7" ];
       switch-to-workspace-8 = [ "<Super>8" ];
       switch-to-workspace-9 = [ "<Super>9" ];
-      switch-to-workspace-10 = [ "<Super>10" ];
+      switch-to-workspace-10 = [ "<Super>0" ];
       switch-to-workspace-left = [ "<Shift><Super>h" ];
       switch-to-workspace-right = [ "<Shift><Super>l" ];
       toggle-fullscreen = [ "<Super>f" ];
+    };
+
+    "org/gnome/desktop/wm/preferences" = {
+      num-workspaces = 10;
     };
 
     "org/gnome/evolution-data-server" = {
@@ -108,7 +152,30 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/mutter" = {
-      output-luminance = [ (mkTuple [ "DP-1" "DEL" "Dell AW3821DW" "#GTIYMxgwABWz" (mkUint32 1) 190.0 ]) ];
+      dynamic-workspaces = false;
+      output-luminance = [
+        (mkTuple [
+          "DP-1"
+          "DEL"
+          "Dell AW3821DW"
+          "#GTIYMxgwABWz"
+          (mkUint32 1)
+          190.0
+        ])
+      ];
+    };
+
+    "org/gnome/nautilus/preferences" = {
+      default-folder-viewer = "icon-view";
+      migrated-gtk-settings = true;
+      search-filter-time-type = "last_modified";
+    };
+
+    "org/gnome/nautilus/window-state" = {
+      initial-size = mkTuple [
+        890
+        550
+      ];
     };
 
     "org/gnome/settings-daemon/plugins/color" = {
@@ -116,11 +183,23 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/settings-daemon/plugins/media-keys" = {
-      screensaver = [];
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+      screensaver = [ ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Super>Return";
+      command = "ghostty";
+      name = "terminal";
     };
 
     "org/gnome/shell" = {
-      enabled-extensions = [ "tactile@lundal.io" "focus-changer@heartmire" ];
+      enabled-extensions = [
+        "tactile@lundal.io"
+        "focus-changer@heartmire"
+      ];
       last-selected-power-profile = "performance";
       welcome-dialog-last-shown-version = "48.4";
     };
@@ -136,10 +215,14 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/shell/keybindings" = {
-      switch-to-application-1 = [];
-      switch-to-application-2 = [];
-      switch-to-application-3 = [];
-      switch-to-application-4 = [];
+      switch-to-application-1 = [ ];
+      switch-to-application-2 = [ ];
+      switch-to-application-3 = [ ];
+      switch-to-application-4 = [ ];
+    };
+
+    "org/gnome/shell/world-clocks" = {
+      locations = [ ];
     };
 
   };
