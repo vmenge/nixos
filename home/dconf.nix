bdce3eb58a173fd6a600cc18ea239492e9d60f77
vmenge <vfmenge@gmail.com>
2025-08-30 07:15:48 +0200

    dconf

--- home/dconf.nix
+++ home/dconf.nix
@@ -11,20 +11,11 @@ with lib.hm.gvariant;
 
     "org/gnome/control-center" = {
       last-panel = "keyboard";
-      window-state = mkTuple [
-        1280
-        784
-        false
-      ];
+      window-state = mkTuple [ 1280 784 false ];
     };
 
     "org/gnome/desktop/app-folders" = {
-      folder-children = [
-        "System"
-        "Utilities"
-        "YaST"
-        "Pardus"
-      ];
+      folder-children = [ "System" "Utilities" "YaST" "Pardus" ];
     };
 
     "org/gnome/desktop/app-folders/folders/Pardus" = {
@@ -34,25 +25,13 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/app-folders/folders/System" = {
-      apps = [
-        "org.gnome.baobab.desktop"
-        "org.gnome.DiskUtility.desktop"
-        "org.gnome.Logs.desktop"
-        "org.gnome.SystemMonitor.desktop"
-      ];
+      apps = [ "org.gnome.baobab.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.Logs.desktop" "org.gnome.SystemMonitor.desktop" ];
       name = "X-GNOME-Shell-System.directory";
       translate = true;
     };
 
     "org/gnome/desktop/app-folders/folders/Utilities" = {
-      apps = [
-        "org.gnome.Connections.desktop"
-        "org.gnome.Evince.desktop"
-        "org.gnome.FileRoller.desktop"
-        "org.gnome.font-viewer.desktop"
-        "org.gnome.Loupe.desktop"
-        "org.gnome.seahorse.Application.desktop"
-      ];
+      apps = [ "org.gnome.Connections.desktop" "org.gnome.Evince.desktop" "org.gnome.FileRoller.desktop" "org.gnome.font-viewer.desktop" "org.gnome.Loupe.desktop" "org.gnome.seahorse.Application.desktop" ];
       name = "X-GNOME-Shell-Utilities.directory";
       translate = true;
     };
@@ -73,13 +52,8 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/input-sources" = {
-      sources = [
-        (mkTuple [
-          "xkb"
-          "us"
-        ])
-      ];
-      xkb-options = [ ];
+      sources = [ (mkTuple [ "xkb" "us" ]) ];
+      xkb-options = [];
     };
 
     "org/gnome/desktop/interface" = {
@@ -108,17 +82,14 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/search-providers" = {
-      sort-order = [
-        "org.gnome.Settings.desktop"
-        "org.gnome.Contacts.desktop"
-        "org.gnome.Nautilus.desktop"
-      ];
+      sort-order = [ "org.gnome.Settings.desktop" "org.gnome.Contacts.desktop" "org.gnome.Nautilus.desktop" ];
     };
 
     "org/gnome/desktop/wm/keybindings" = {
       close = [ "<Super>q" ];
-      minimize = [ ];
+      minimize = [];
       move-to-workspace-1 = [ "<Shift><Super>1" ];
+      move-to-workspace-10 = [ "<Shift><Super>0" ];
       move-to-workspace-2 = [ "<Shift><Super>2" ];
       move-to-workspace-3 = [ "<Shift><Super>3" ];
       move-to-workspace-4 = [ "<Shift><Super>4" ];
@@ -127,8 +98,8 @@ with lib.hm.gvariant;
       move-to-workspace-7 = [ "<Shift><Super>7" ];
       move-to-workspace-8 = [ "<Shift><Super>8" ];
       move-to-workspace-9 = [ "<Shift><Super>9" ];
-      move-to-workspace-10 = [ "<Shift><Super>0" ];
       switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-10 = [ "<Super>0" ];
       switch-to-workspace-2 = [ "<Super>2" ];
       switch-to-workspace-3 = [ "<Super>3" ];
       switch-to-workspace-4 = [ "<Super>4" ];
@@ -137,7 +108,6 @@ with lib.hm.gvariant;
       switch-to-workspace-7 = [ "<Super>7" ];
       switch-to-workspace-8 = [ "<Super>8" ];
       switch-to-workspace-9 = [ "<Super>9" ];
-      switch-to-workspace-10 = [ "<Super>0" ];
       switch-to-workspace-left = [ "<Shift><Super>h" ];
       switch-to-workspace-right = [ "<Shift><Super>l" ];
       toggle-fullscreen = [ "<Super>f" ];
@@ -151,18 +121,23 @@ with lib.hm.gvariant;
       migrated = true;
     };
 
+    "org/gnome/gnome-system-monitor" = {
+      cpu-colors = [ (mkTuple [ (mkUint32 0) "#e01b24" ]) (mkTuple [ 1 "#ff7800" ]) (mkTuple [ 2 "#f6d32d" ]) (mkTuple [ 3 "#33d17a" ]) (mkTuple [ 4 "#26a269" ]) (mkTuple [ 5 "#62a0ea" ]) (mkTuple [ 6 "#1c71d8" ]) (mkTuple [ 7 "#613583" ]) (mkTuple [ 8 "#9141ac" ]) (mkTuple [ 9 "#c061cb" ]) (mkTuple [ 10 "#ffbe6f" ]) (mkTuple [ 11 "#f9f06b" ]) (mkTuple [ 12 "#8ff0a4" ]) (mkTuple [ 13 "#2ec27e" ]) (mkTuple [ 14 "#1a5fb4" ]) (mkTuple [ 15 "#c061cb" ]) (mkTuple [ 16 "#7999c483f332" ]) (mkTuple [ 17 "#e7fff3327999" ]) (mkTuple [ 18 "#dae97999f332" ]) (mkTuple [ 19 "#7999f332b76e" ]) (mkTuple [ 20 "#f33293f37999" ]) (mkTuple [ 21 "#799982baf332" ]) (mkTuple [ 22 "#a635f3327999" ]) (mkTuple [ 23 "#f3327999c9b1" ]) ];
+      maximized = false;
+      show-dependencies = false;
+      show-whose-processes = "user";
+      window-height = 720;
+      window-width = 800;
+    };
+
+    "org/gnome/gnome-system-monitor/proctree" = {
+      col-26-visible = false;
+      col-26-width = 0;
+    };
+
     "org/gnome/mutter" = {
       dynamic-workspaces = false;
-      output-luminance = [
-        (mkTuple [
-          "DP-1"
-          "DEL"
-          "Dell AW3821DW"
-          "#GTIYMxgwABWz"
-          (mkUint32 1)
-          190.0
-        ])
-      ];
+      output-luminance = [ (mkTuple [ "DP-1" "DEL" "Dell AW3821DW" "#GTIYMxgwABWz" (mkUint32 1) 190.0 ]) ];
     };
 
     "org/gnome/nautilus/preferences" = {
@@ -172,10 +147,7 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/nautilus/window-state" = {
-      initial-size = mkTuple [
-        890
-        550
-      ];
+      initial-size = mkTuple [ 711 916 ];
     };
 
     "org/gnome/settings-daemon/plugins/color" = {
@@ -183,10 +155,8 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/settings-daemon/plugins/media-keys" = {
-      custom-keybindings = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-      ];
-      screensaver = [ ];
+      custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
+      screensaver = [];
     };
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
@@ -196,33 +166,85 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/shell" = {
-      enabled-extensions = [
-        "tactile@lundal.io"
-        "focus-changer@heartmire"
-      ];
+      disabled-extensions = [];
+      enabled-extensions = [ "tactile@lundal.io" "focus-changer@heartmire" "Vitals@CoreCoding.com" "blur-my-shell@aunetx" ];
       last-selected-power-profile = "performance";
       welcome-dialog-last-shown-version = "48.4";
     };
 
+    "org/gnome/shell/extensions/blur-my-shell" = {
+      settings-version = 2;
+    };
+
+    "org/gnome/shell/extensions/blur-my-shell/appfolder" = {
+      brightness = 0.6;
+      sigma = 30;
+    };
+
+    "org/gnome/shell/extensions/blur-my-shell/coverflow-alt-tab" = {
+      pipeline = "pipeline_default";
+    };
+
+    "org/gnome/shell/extensions/blur-my-shell/dash-to-dock" = {
+      blur = true;
+      brightness = 0.6;
+      pipeline = "pipeline_default_rounded";
+      sigma = 30;
+      static-blur = true;
+      style-dash-to-dock = 0;
+    };
+
+    "org/gnome/shell/extensions/blur-my-shell/lockscreen" = {
+      pipeline = "pipeline_default";
+    };
+
+    "org/gnome/shell/extensions/blur-my-shell/overview" = {
+      pipeline = "pipeline_default";
+    };
+
+    "org/gnome/shell/extensions/blur-my-shell/panel" = {
+      brightness = 0.6;
+      pipeline = "pipeline_default";
+      sigma = 30;
+    };
+
+    "org/gnome/shell/extensions/blur-my-shell/screenshot" = {
+      pipeline = "pipeline_default";
+    };
+
+    "org/gnome/shell/extensions/blur-my-shell/window-list" = {
+      brightness = 0.6;
+      sigma = 30;
+    };
+
     "org/gnome/shell/extensions/tactile" = {
       col-0 = 0;
-      layout-2-col-0 = 2;
-      layout-2-col-2 = 0;
-      layout-2-col-3 = 0;
-      monitor-0-layout = 1;
+      gap-size = 12;
+      layout-2-col-0 = 1;
+      layout-2-col-1 = 1;
+      layout-2-col-2 = 1;
+      layout-2-col-3 = 1;
+      layout-2-row-2 = 1;
+      monitor-0-layout = 2;
       row-0 = 1;
       show-tiles = [ "<Super>e" ];
     };
 
+    "org/gnome/shell/extensions/vitals" = {
+      hot-sensors = [ "_memory_usage_" "_system_load_1m_" "_processor_usage_" "__temperature_avg__" "__network-rx_max__" "__network-tx_max__" ];
+      icon-style = 1;
+      show-battery = false;
+    };
+
     "org/gnome/shell/keybindings" = {
-      switch-to-application-1 = [ ];
-      switch-to-application-2 = [ ];
-      switch-to-application-3 = [ ];
-      switch-to-application-4 = [ ];
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
     };
 
     "org/gnome/shell/world-clocks" = {
-      locations = [ ];
+      locations = [];
     };
 
   };
