9680c58359ebfaab2d58d3a00f9f4c6816789046
vmenge <vfmenge@gmail.com>
2025-09-09 10:41:16 +0200

    no custom xdg entry for spotify

--- home/apps.nix
+++ home/apps.nix
@@ -3,20 +3,20 @@
 
 {
   xdg.desktopEntries = {
-    spotify = {
-      name = "Spotify";
-      genericName = "Music Player";
-      icon = "spotify-client";
-      exec = "${pkgs.spotify}/bin/spotify --enable-features=UseOzonePlatform --ozone-platform=wayland %U";
-      terminal = false;
-      mimeType = [ "x-scheme-handler/spotify" ];
-      categories = [
-        "Audio"
-        "Music"
-        "Player"
-        "AudioVideo"
-      ];
-    };
+    # spotify = {
+    #   name = "Spotify";
+    #   genericName = "Music Player";
+    #   icon = "spotify-client";
+    #   exec = "${pkgs.spotify}/bin/spotify --enable-features=UseOzonePlatform --ozone-platform=wayland %U";
+    #   terminal = false;
+    #   mimeType = [ "x-scheme-handler/spotify" ];
+    #   categories = [
+    #     "Audio"
+    #     "Music"
+    #     "Player"
+    #     "AudioVideo"
+    #   ];
+    # };
   };
 
   xdg.mimeApps = {
