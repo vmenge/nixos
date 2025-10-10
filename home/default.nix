a5837de3305543cb1deb96cd34fff90d47f4fcd8
vmenge <vfmenge@gmail.com>
2025-10-10 19:22:19 +0200

    Revert "exercism dir"
    This reverts commit 57574c4c9b707e79977fa416695a4d503ab02608.

--- home/default.nix
+++ home/default.nix
@@ -54,9 +54,6 @@ in
 
     ".config/sunshine/sunshine.conf".source = sl ".config/sunshine/sunshine.conf";
     ".config/sunshine/apps.json".source = sl ".config/sunshine/apps.json";
-
-    ".config/exercism".source = sl ".config/exericsm";
-    ".config/exercism".recursive = true;
   };
 
   home.sessionVariables = {
