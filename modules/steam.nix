242ce9d06b56a84c34d39225313e88f213698c92
vmenge <vfmenge@gmail.com>
2026-01-06 20:04:28 +0100

    opencomposite

--- modules/steam.nix
+++ modules/steam.nix
@@ -55,4 +55,23 @@ in
       };
     }
   ];
+
+  services.wivrn = {
+    enable = true;
+    openFirewall = true;
+
+    # Write information to /etc/xdg/openxr/1/active_runtime.json, VR applications
+    # will automatically read this and work with WiVRn (Note: This does not currently
+    # apply for games run in Valve's Proton)
+    defaultRuntime = true;
+
+    # Run WiVRn as a systemd service on startup
+    autoStart = true;
+
+    # If you're running this with an nVidia GPU and want to use GPU Encoding (and don't otherwise have CUDA enabled system wide), you need to override the cudaSupport variable.
+    package = (pkgs.wivrn.override { cudaSupport = true; });
+
+    # You should use the default configuration (which is no configuration), as that works the best out of the box.
+    # However, if you need to configure something see https://github.com/WiVRn/WiVRn/blob/master/docs/configuration.md for configuration options and https://mynixos.com/nixpkgs/option/services.wivrn.config.json for an example configuration.
+  };
 }
