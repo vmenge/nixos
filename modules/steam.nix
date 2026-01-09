205a578c269bae0b50212f412a2975586399df4c
vmenge <vfmenge@gmail.com>
2026-01-09 17:16:44 +0100

    remove vr workarounds

--- modules/steam.nix
+++ modules/steam.nix
@@ -34,25 +34,4 @@ in
     enable = true;
     enable32Bit = true;
   };
-
-  # vr shit
-  security.pam.loginLimits = [
-    {
-      domain = "vmenge";
-      type = "-";
-      item = "rtprio";
-      value = "99";
-    }
-  ];
-
-  boot.kernelPatches = [
-    {
-      name = "amdgpu-ignore-ctx-privileges";
-      patch = pkgs.fetchpatch {
-        name = "cap_sys_nice_begone.patch";
-        url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
-        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
-      };
-    }
-  ];
 }
