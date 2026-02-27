b1f71af8ba5eab7f290e50fb4f1d34f6df58916d
vmenge <vfmenge@gmail.com>
2026-02-27 13:25:37 +0100

    nixos: select internal mic on fw13tfh

--- hosts/fw13tfh/default.nix
+++ hosts/fw13tfh/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -9,4 +9,21 @@
   networking.hostName = "fw13tfh";
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
+
+  # Ensure the internal mic is selected on this host (ALC285 exclusive capture group).
+  systemd.services.fw13tfh-internal-mic = {
+    description = "Select internal microphone on ALC285";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "sound.target" ];
+    path = [ pkgs.alsa-utils ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = ''
+      amixer -c 1 set 'Headset Mic' nocap
+      amixer -c 1 set 'Internal Mic' cap
+      amixer -c 1 set 'Internal Mic Boost' 2
+    '';
+  };
 }
