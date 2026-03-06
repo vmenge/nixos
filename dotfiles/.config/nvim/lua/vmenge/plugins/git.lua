70da10ae72600a6065f71721160d0cacfeb10353
vmenge <vfmenge@gmail.com>
2026-03-06 11:13:07 +0100

    fix(nvim): gitlink

--- dotfiles/.config/nvim/lua/vmenge/plugins/git.lua
+++ dotfiles/.config/nvim/lua/vmenge/plugins/git.lua
@@ -1,11 +1,9 @@
-require("lazy").setup({
-  {
-    "linrongbin16/gitlinker.nvim",
-    cmd = "GitLink",
-    opts = {},
-    keys = {
-      { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
-      { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
-    },
+return {
+  "linrongbin16/gitlinker.nvim",
+  cmd = "GitLink",
+  opts = {},
+  keys = {
+    { "<leader>gy", "<cmd>GitLink<cr>",  mode = { "n", "v" }, desc = "Yank git link" },
+    { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
   },
-})
+}
