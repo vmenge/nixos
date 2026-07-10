4af4b0cf76637821e1b1a4d955486db96cbb0e3e
vmenge <vfmenge@gmail.com>
2026-07-10 22:38:02 +0200

    zed;treesitter

--- dotfiles/.config/nvim/lua/vmenge/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/vmenge/plugins/treesitter.lua
@@ -97,5 +97,10 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+      max_lines = 2,
+      multiline_threshold = 2,
+      trim_scope = "inner",
+    },
   },
 }
